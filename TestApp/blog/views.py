ef0635a2126a9966e40c96b59a42f8cef6be7a26
nkaka23 <nkakaeric96@gmail.com>
2021-04-16 05:26:46 +0200

    new serializer

--- TestApp/blog/views.py
+++ TestApp/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import Userserializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserView(viewsets.ModelViewSet):
+    serializer_class=Userserializer
+    queryset=User.objects.all().order_by('-date_joined')
